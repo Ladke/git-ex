0d4dd8eebe9e5f63d137da6c87e3c1c16c2f8884
elad <eladozy@gmail.com>
2018-10-13 12:47:54 +0300

    v0.1

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function renderProjs() {
     gProjs.forEach(function (proj) {
         strHtml += `
            <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" onclick="openModal('${proj.id}')" href="#portfolioModal">
+            <a class="portfolio-link" data-toggle="modal"  href="#portfolioModal">
             <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -64,41 +64,3 @@ function renderProjs() {
 }
 
 
-function openModal(projId) {
-    proj = getProjById(projId)
-    var strHtml = '';
-
-    strHtml += `
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="close-modal" data-dismiss="modal">
-                <div class="lr">
-                <div class="rl"></div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <div class="modal-body">
-                    <!-- Project Details Go Here -->
-                    <h2>${proj.name}</h2>
-                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                    <img class="img-fluid d-block mx-auto" src="${proj.img}" alt="">
-                    <p>${proj.desc}</p>
-                    <ul class="list-inline">
-                        <li>Date: January 2017</li>
-                        <li>Client: Threads</li>
-                        <li>Category: Illustration</li>
-                    </ul>
-                    <button class="btn btn-primary" data-dismiss="modal" type="button">
-                        <i class="fa fa-times"></i>
-                        Close Project</button>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>`
-
-        $('#portfolioModal').html(strHtml);
-}
